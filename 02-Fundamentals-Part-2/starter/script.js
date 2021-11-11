fcd9851e30f1f9141d1a928bda98676ecc65dbea
trebuh1996 <trebuh1996@gmail.com>
2021-11-11 22:24:54 +0100

    added lesson 45

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -299,7 +299,7 @@ hubert["twitter"] = "@dupawołowa"
 console.log(hubert.friends.length + 1)
 
 
-*/
+
 
 //++++++++++++++++++++++++++++++++++++++++++
 //lesson 44 - object methods
@@ -342,8 +342,53 @@ console.log(hubert.age)
 // console.log(hubert['calcAge'](1996))
 
 
-
+*/
 
 //++++++++++++++++++++++++++++++++++++++++++
 //lesson 45 - coding challenge #3
  //++++++++++++++++++++++++++++++++++++++++++
+
+//  Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+// 1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+// 3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+// TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+// GOOD LUCK 😀
+
+const Mark = {
+    firstName: "Mark",
+    lastName: "Miller",
+    mass: 78,
+    height: 1.69,
+    
+    calcBMI: function(){
+        this.BMI = this.mass / this.height ** 2 
+        return this.BMI 
+    }
+}
+
+
+const John = {
+    firstName: "John",
+    lastName: "Smith",
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function(){
+        this.BMI = this.mass / this.height ** 2 
+        return this.BMI 
+    }
+}
+
+console.log(Mark.calcBMI())
+console.log(John.calcBMI())
+
+console.log(`${Mark.firstName} ${Mark.lastName} BMI(${Mark.BMI}) is ${Mark.BMI > John.BMI ? "greater" : "smaller"} than ${John.firstName} ${John.lastName} BMI(${John.BMI})`)
+
+
+// console.log(Mark)
+// console.log(John)
+
