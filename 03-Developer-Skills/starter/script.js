498854cf229fbf0490491fc739c13aab81184381
trebuh1996 <trebuh1996@gmail.com>
2021-11-13 18:08:14 +0100

    added lesson 59

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,72 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+'use strict'
 
+/*
+
+//++++++++++++++++++++++++++++++++++++++++++
+//lesson 55 - adjusting the vs code
+//++++++++++++++++++++++++++++++++++++++++++
+const xyzabc = 23
+const y = 1
+
+const calcAge = birthYear => 2037 - birthYear
+console.log(calcAge(1996))
+
+//FIXME
+//BUG
+//TODO
+
+//++++++++++++++++++++++++++++++++++++++++++
+//lesson 56 - installing the node.js, setting dev environment
+//++++++++++++++++++++++++++++++++++++++++++
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++
+//lesson 59 - Using Google, StackOverflow and MDN
+//++++++++++++++++++++++++++++++++++++++++++
+
+const temp0 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5]
+const temp1 = [1, 5, 7]
+// const temp2 = [2, 6, 8]
+
+const calcTempAmp = function (temp0) {
+    let max = temp0[0]
+    let min = temp0[0]
+    for (let i = 0; i < temp0.length; i++) {
+        if (typeof temp0[i] !== 'number') continue
+        if (temp0[i] > max) max = temp0[i]
+        if (temp0[i] < min) min = temp0[i]
+    }
+    return max - min
+}
+
+const calcTempAmpNew = function (temp0, temp1) {
+    const array1 = ['a', 'b', 'c']
+    const array2 = ['d', 'e', 'f']
+    const array3 = array1.concat(array2)
+
+    const temps = temp0.concat(temp1)
+    console.log(temps)
+
+    // const
+    let max = temps[0]
+    let min = temps[0]
+    for (let i = 0; i < temps.length; i++) {
+        if (typeof temps[i] !== 'number') continue
+        if (temps[i] > max) max = temps[i]
+        if (temps[i] < min) min = temps[i]
+    }
+    return max - min
+}
+
+const amplitudeNew = calcTempAmpNew([3, 5, 1], [9, 0, 5])
+console.log(amplitudeNew)
+
+
+
+*/
+
+//++++++++++++++++++++++++++++++++++++++++++
+//lesson 60 - Debugging (Fixing Errors)
+//++++++++++++++++++++++++++++++++++++++++++
