fde25f9bf442139805c8c23cec5b1d08e09710da
trebuh1996 <trebuh1996@gmail.com>
2021-11-15 12:33:37 +0100

    added lesson 62 - coding cahllenge #1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -65,8 +65,70 @@ console.log(amplitudeNew)
 
 
 
-*/
+
 
 //++++++++++++++++++++++++++++++++++++++++++
 //lesson 60 - Debugging (Fixing Errors)
 //++++++++++++++++++++++++++++++++++++++++++
+
+//++++++++++++++++++++++++++++++++++++++++++
+//lesson 61 - Debugging with the console and breakpoints
+//++++++++++++++++++++++++++++++++++++++++++
+
+//in console, in the sources card you can add an breakpoints to code
+
+const measureKelvin = function () {
+    const measurment = {
+        type: 'temp',
+        unit: 'celsius',
+        //value: Number(prompt('Degrees celsius:')),
+        value: 10,
+    }
+    // B) Find the bug
+    console.log(measurment)
+    // console.table(measurment)
+
+    const kelvin = measurment.value + 273
+    return kelvin
+}
+
+// A) IDENTIFY
+console.log(measureKelvin())
+// console.warn(measureKelvin())
+// console.error(measureKelvin())
+
+
+*/
+
+//++++++++++++++++++++++++++++++++++++++++++
+//lesson 62 - Coding challenge #1
+//++++++++++++++++++++++++++++++++++++++++++
+
+// Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+// Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+// Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+// Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+// TEST DATA 1: [17, 21, 23]
+// TEST DATA 2: [12, 5, -5, 0, 4]
+
+const forecastArr1 = [17, 21, 23]
+const forecastArr2 = [12, 5, -5, 0, 4]
+
+const printForecast = function (arr) {
+    let assemblyText = ''
+    let text = ''
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        text = `... ${arr[i]}ºC in ${i + 1} days `
+        assemblyText = assemblyText + text
+    }
+
+    return assemblyText
+}
+
+console.log(printForecast(forecastArr1))
+console.log(printForecast(forecastArr2))
