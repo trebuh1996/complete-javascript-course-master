93b9caad2beba45743b360a27ca50c2c7a087c9a
trebuh1996 <trebuh1996@gmail.com>
2021-11-24 20:21:42 +0100

    added lesson 80 - using 'hidden' to hide/show things on website

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -6,6 +6,29 @@ const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 
-console.log(btnsOpenModal)
+// console.log(btnsOpenModal)
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//     console.log(
+//         btnsOpenModal[i].addEventListener('click', function () {
+//             console.log('Button clicked')
+//             modal.classList.remove('hidden')
+//             overlay.classList.remove('hidden') // blur site
+//         })
+//     )
+
+const showModal = function () {
+    console.log('Button clicked')
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden') // blur site
+}
+const closeModal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+// console.log(btnsOpenModal)
 for (let i = 0; i < btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent)
+    console.log(btnsOpenModal[i].addEventListener('click', showModal))
+
+btnCloseModal.addEventListener('click', closeModal) //here you dont use () after calling function!
+overlay.addEventListener('click', closeModal) //here you dont use () after calling function!
